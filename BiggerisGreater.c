668584f1ec9162eeae483ed05e73239ae878fead
aadityarohan <aaditya.rohan@yahoo.com>
2022-08-07 20:47:53 +0530

    lexographic arrangment

--- BiggerisGreater.c
+++ BiggerisGreater.c
@@ -3,23 +3,23 @@
 int biggerIsGreater(char w[])
 {
     int i = strlen(w) - 1;
-    while (i > 0 && w[i - 1] >= w[i])
-        i--;
+    while (i > 0 && w[i - 1] >= w[i])       //it checks if there exists an alphabet which is smaller than it's previous
+        i--;                                   // if this exists the loop is broken with pivot at i-1
     if (i <= 0)
     {
-        printf("no answer\n");
+        printf("no answer\n");              // if no such as above mentioned exists then no further possibility is there
         return 0;
     }
-    int j = strlen(w) - 1;
-    while (w[j] <= w[i - 1])
+    int j = strlen(w) - 1;                  // we check the suffix from pivot to find the largest alphabet within the suffix
+    while (w[j] <= w[i - 1])                //the loop breaks when greater alphabet is found
         j--;
-    char temp = w[i - 1];
+    char temp = w[i - 1];                   //pivot alphabet is swapped with largest aplhabet in the suffix
     w[i - 1] = w[j];
     w[j] = temp;
     j = strlen(w) - 1;
-    while (i < j)
+    while (i < j)                           // the suffix is sorted in ascending order from the alphabet next to pivot till end
     {
-        temp = w[i];
+        temp = w[i];                    
         w[i] = w[j];
         w[j] = temp;
         i++;
